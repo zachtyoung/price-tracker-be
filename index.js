bf90ed1fc1316a23c725c85af44c6af9d9cffe29
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2020-02-15 23:55:04 -0600

    Sanity Commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const server = require('./server.js');
 
-server.listen(4000, () => {
-  console.log('\n* Server Running on http://localhost:4000 *\n');
-});
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
