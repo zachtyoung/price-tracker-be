97a1d51e79b86f85f7cb42af134f85e4944645a5
Troy Young <troyyoung@Troys-MacBook-Pro.local>
2020-02-19 01:55:47 -0600

    working locally on second mac

--- data/migrations/20200113015219_accounts.js
+++ data/migrations/20200113015219_accounts.js
@@ -2,19 +2,21 @@
 exports.up = function(knex) {
     return knex.schema
     .createTable('users', function(users) {
-      users.increments('id');
+      users.increments();
       users.string('name').notNullable();
       users.string('email').notNullable().unique();
       users.biginteger('phone').notNullable().unique();
       users.string('password').notNullable();
     })
     .createTable('products', function(products) {
-        products.increments('id');
+        products.increments();
         products.integer('user_id').unsigned().notNullable().references('id').inTable('users');
         products.string('url', 1024).notNullable();
       })
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('users').dropTableIfExists('products');
+    return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('products');
 };
